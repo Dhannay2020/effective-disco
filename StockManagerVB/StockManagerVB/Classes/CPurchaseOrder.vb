14b44ab598c86138ed7b3d33cc6e601d267cea48
Darren Hannay <Dhannay@buckscollegegroup.ac.uk>
2023-01-24 16:18:08 +0000

    Save to Purchase Order Class
    Added save function to the Purchase order class for both head and line table

--- StockManagerVB/StockManagerVB/Classes/CPurchaseOrder.vb
+++ StockManagerVB/StockManagerVB/Classes/CPurchaseOrder.vb
@@ -1,3 +1,61 @@
-﻿Public Class CPurchaseOrder
+﻿Imports System.Data.SqlClient
+Public Class CPurchaseOrder
+    Inherits CUtility
 
+    Public Function SavePOrdersHead(notes As String, invoice As String, shipper As String, shipperinv As String) As Integer
+
+        Using conn As New SqlConnection(GetConnString(1))
+            Dim insCmd As New SqlCommand
+            With insCmd
+                .Connection = conn
+                .CommandText = "INSERT INTO tblPurchaseOrder (OurRef, SupplierRef, LocationRef,  TotalGarments, TotalBoxes, TotalHangers, NetAmount, DeliveryCharge, Commission, TotalAmount, DeliveryDate, DeliveryType, ConfirmedDate, Notes, InvoiceNo, Shipper, ShipperInvoice, CreatedBy, CreatedDate) VALUES (@OurRef, @SupplierRef, @LocationRef, @TotalGarments, @TotalBoxes, @TotalHangers, @NetAmount, @DeliveryCharge, @Commission, @TotalAmount, @DeliveryDate, @DeliveryType, @ConfirmedDate, @Notes, @InvoiceNo, @Shipper, @ShipperInvoice, @CreatedBy, @CreatedDate)"
+                .CommandType = CommandType.Text
+                With .Parameters
+                    .AddWithValue("@OurRef", StockCode)
+                    .AddWithValue("@SupplierRef", SupplierRef)
+                    .AddWithValue("@LocationRef", WarehouseRef)
+                    .AddWithValue("@TotalGarments", TotalGarments)
+                    .AddWithValue("@TotalBoxes", TotalBoxes)
+                    .AddWithValue("@TotalHangers", TotalHangers)
+                    .AddWithValue("@NetAmount", NetAmount)
+                    .AddWithValue("@DeliveryCharge", DeliveryCharge)
+                    .AddWithValue("@Commission", Commission)
+                    .AddWithValue("@TotalAmount", TotalAmount)
+                    .AddWithValue("@DeliveryDate", NewDate)
+                    .AddWithValue("@DeliveryType", "Confirmed")
+                    .AddWithValue("@ConfirmedDate", NewDate)
+                    .AddWithValue("@Notes", notes)
+                    .AddWithValue("@InvoiceNo", invoice)
+                    .AddWithValue("@Shipper", shipper)
+                    .AddWithValue("@ShipperInvoice", shipperinv)
+                    .AddWithValue("@CreatedBy", GetUserName())
+                    .AddWithValue("@CreatedDate", Now)
+                End With
+                .Connection.Open()
+                Result = .ExecuteNonQuery()
+            End With
+        End Using
+        Return Result
+    End Function
+    Public Function SavePOrdersLine() As Boolean
+        Using conn As New SqlConnection(GetConnString(1))
+            Dim insCmd As New SqlCommand
+            With insCmd
+                .Connection = conn
+                .CommandText = "INSERT INTO tblPurchaseOrderLine (DeliveryID, StockCode, DQtyGarments, DQtyBoxes, DQtyHangers, NetAmount) VALUES (@DeliveryID, @StockCode, @DQtyGarments, @DQtyBoxes, @DQtyHangers, @NetAmount)"
+                .CommandType = CommandType.Text
+                With .Parameters
+                    .AddWithValue("@DeliveryID", DeliveryID)
+                    .AddWithValue("@StockCode", StockCode)
+                    .AddWithValue("@DQtyGarments", DeliveredQtyGarments)
+                    .AddWithValue("@DQtyBoxes", DeliveredQtyBoxes)
+                    .AddWithValue("@DQtyHangers", DeliveredQtyHangers)
+                    .AddWithValue("@NetAmount", LineAmount)
+                End With
+                .Connection.Open()
+                .ExecuteNonQuery()
+            End With
+        End Using
+        Return True
+    End Function
 End Class
--- StockManagerVB/StockManagerVB/Classes/CUtility.vb
+++ StockManagerVB/StockManagerVB/Classes/CUtility.vb
@@ -1,8 +1,11 @@
 ﻿Public Class CUtility
     Public WarehouseRef, ShopRef, SupplierRef, StockCode As String
     Public AddressLine1, AddressLine2, AddressLine3, AddressLine4, PostCode, Telephone, Fax, ContactName, EMail, WebsiteAddress, Memo As String
-
-
+    Public Result As Integer
+    Public SeasonID, DeliveryID, WarehouseAdjustmentID, WarehouseTransferID, WarehouseReturnID, ShopDeliveryID, ShopAdjustmentID, ShopTransferID, ShopSaleID, ShopReturnID As Integer
+    Public DeliveredQtyHangers, DeliveredQtyBoxes, DeliveredQtyGarments, TotalGarments, TotalHangers, TotalBoxes As Integer
+    Public LineAmount, NetAmount, Commission, DeliveryCharge, VATAmount, TotalAmount As Decimal
+    Public oldDate, NewDate As Date
     Public Function GetConnString(ByVal Id As Integer) As String
         Dim StringConn As String
         If Id = 1 Then
